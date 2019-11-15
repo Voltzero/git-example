74dc00f2d7766fe8a9e02edeefd9af7e204cf59c
majkel <mio311@wp.pl>
2019-11-15 15:50:44 +0100

    [fix] - deleting message

--- src/main/java/pl/mp/git_examples/Hello.java
+++ src/main/java/pl/mp/git_examples/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
 
-        System.out.println("Hello my friend");
         System.out.println("Hello my friend again!");
         System.out.println("hasta la vista");
         System.out.println("jeszcze cos");
