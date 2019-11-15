3e22f602779604186017e77af554f2daa03319a2
majkel <mio311@wp.pl>
2019-11-15 11:44:06 +0100

    adding AllArgsConstr

--- src/main/java/pl/mp/git_examples/car/model/Car.java
+++ src/main/java/pl/mp/git_examples/car/model/Car.java
@@ -1,10 +1,12 @@
 package pl.mp.git_examples.car.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class Car {
     //poczytać o hashcodach kontraktach
     private String brand;
