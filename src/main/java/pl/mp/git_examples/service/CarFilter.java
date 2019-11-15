de679a93df9789452dbbe24f6167a095033f39ca
majkel <mio311@wp.pl>
2019-11-15 14:24:05 +0100

    [change] - Lambda stream filter methods

--- src/main/java/pl/mp/git_examples/service/CarFilter.java
+++ src/main/java/pl/mp/git_examples/service/CarFilter.java
@@ -2,12 +2,28 @@ package pl.mp.git_examples.service;
 
 import pl.mp.git_examples.car.model.Car;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarFilter {
 
     public List<Car> findCarOfColor(List<Car> cars, String color) {
+        List<Car> result = new ArrayList<>();
+        for (Car car:cars) {
+            if (car.getColor().equals(color)) {
+                result.add(car);
+            }
+        }
+        return result;
+    }
 
-        return null;
+    public List<Car> findCarOfColorJava8(List<Car> cars, String color) {
+
+        return cars.stream()
+                .filter(car -> car.getColor().equals(color))
+                .collect(Collectors.toList());
+        //return Collections.emptyList();
     }
 }
