f572f3572f38117630e224308ee6f9ce3f69b252
majkel <mio311@wp.pl>
2019-11-15 14:41:50 +0100

    [change] - main

--- src/main/java/pl/mp/git_examples/service/CarFilter.java
+++ src/main/java/pl/mp/git_examples/service/CarFilter.java
@@ -5,11 +5,12 @@ import pl.mp.git_examples.car.model.Car;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class CarFilter {
 
-    public List<Car> findCarOfColor(List<Car> cars, String color) {
+    static public List<Car> findCarOfColor(List<Car> cars, String color) {
         List<Car> result = new ArrayList<>();
         for (Car car : cars) {
             if (car.getColor().equals(color)) {
@@ -19,7 +20,7 @@ public class CarFilter {
         return result;
     }
 
-    public List<Car> findCarOfColorJava8(List<Car> cars, String color) {
+    static public List<Car> findCarOfColorJava8(List<Car> cars, String color) {
 
         return cars.stream()
                 .filter(car -> car.getColor().equals(color))
@@ -27,11 +28,22 @@ public class CarFilter {
         //return Collections.emptyList();
     }
 
-    public List<Car> getCarsWithGIvenEngineAndPriceLessThan(List<Car> cars, BigDecimal maxPrice, int minimalEngine) {
+    static public List<Car> getCarsWithGIvenEngineAndPriceLessThan(List<Car> cars, BigDecimal maxPrice, int minimalEngine) {
 
         return cars.stream()
                 .filter(car -> car.getPrice().compareTo(maxPrice) < 0 && car.getEngine() <= minimalEngine)
                 .collect(Collectors.toList());
     }
 
+    static public List<Car> filterCars(List<Car> cars, Predicate<Car> myFilter) {
+
+        return cars.stream()
+                .filter(myFilter)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        filterCars(CarProvider.getMyCars(), car -> car.getBrand().equals("Renault"))
+                .forEach(car -> System.out.println(car));
+    }
 }
